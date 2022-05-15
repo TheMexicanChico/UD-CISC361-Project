bc7e80129c65a222146cae1fecd877c133abcc36
Abel Juarez <jabel@udel.edu>
2022-05-15 15:30:03 -0400

    Moved code around

--- structure.h
+++ structure.h
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+
+typedef struct config {
+   int arrivalTime;
+   int memory;
+   int devices;
+   int quantumTime;
+}config;
+
+typedef struct job {
+   int jobID;
+   int priority;
+   int arrivalTime;
+   int memory;
+   int devices;
+   int runTime;
+}job;
+
+typedef struct request {
+   int timeStamp;
+   int jobID;
+   int devices;
+}request;
+
+typedef struct release {
+   int timeStamp;
+   int jobID;
+   int devices; 
+}release;
 
 void initialize_config(config c[], int size);
 void initialize_Job(job c[], int size);
