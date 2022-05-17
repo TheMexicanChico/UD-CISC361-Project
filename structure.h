fcd51476f4f0026c8104a54d421881aa53ff6a20
Abel Juarez <jabel@udel.edu>
2022-05-16 22:15:28 -0400

    These still need work

--- structure.h
+++ structure.h
@@ -16,21 +16,25 @@ typedef struct job {
    int memory;
    int devices;
    int runTime;
+   struct job* next;
 }job;
-
+/*
 typedef struct request {
    int timeStamp;
    int jobID;
    int devices;
+   struct request* next;
 }request;
 
 typedef struct release {
    int timeStamp;
+   int memory;
    int jobID;
    int devices; 
+   struct release* next;
 }release;
-
-void initialize_config(config c[], int size);
-void initialize_Job(job c[], int size);
-void initialize_request(request c[], int size);
-void initialize_release(release c[], int size);
+*/
+void initialize_config(config* head, int size);
+void initialize_Job(job* head, int size);
+void initialize_request(request* head, int size);
+void initialize_release(release* head, int size);
